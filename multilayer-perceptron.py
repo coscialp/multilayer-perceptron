102ebe96a1109525c26d904ae42a42dc83b1160f
Corentin Scialpi <coscialp@z2r12p4.42lyon.fr>
2022-05-09 17:49:15 +0200

    save model

--- multilayer-perceptron.py
+++ multilayer-perceptron.py
@@ -1,6 +1,7 @@
+import json
 import sys, os
 import matplotlib.pyplot as plt 
-from matplotlib.pyplot import figure
+import numpy as np
 from AI_learn.neural_network import MLPClassifier
 from AI_learn.dataset import load_dataset
 
@@ -69,6 +70,39 @@ def print_metrics(ax, fig, model, name_of_model, save):
         print(line)
     print('-' * 100)
 
+
+def save_model(best_model):
+    with open('model.npy', 'wb') as file:
+        np.save(file, best_model.hidden_layers_)
+        np.save(file, len(best_model.parameters_) // 2)
+        np.save(file, best_model.normalize_)
+        if best_model.normalize_ == True:
+            np.save(file, best_model.normalize_mean_)
+            np.save(file, best_model.normalize_std_)
+        for c in range(1, len(best_model.parameters_) // 2):
+            np.save(file, best_model.parameters_[f'W{c}'])
+            np.save(file, best_model.parameters_[f'b{c}'])
+
+
+def load_model():
+    hidden_layer = None
+    mean = None
+    std = None
+    parameters = {}
+    with open('model.npy', 'rb') as file:
+        hidden_layer = np.load(file)
+        C = np.load(file)
+        normalize = np.load(file)
+        if normalize == True:
+            mean = np.load(file)
+            std = np.load(file)
+        for c in range(1, C):
+            parameters[f'W{c}'] = np.load(file)
+            parameters[f'b{c}'] = np.load(file)
+
+    return (hidden_layer, mean, std, parameters)
+
+
 if __name__ == '__main__':
     save = verif_arg()
         
@@ -88,9 +122,33 @@ if __name__ == '__main__':
         RMSprop.fit(X, y, solver='RMSprop', random_state=0)
 
         fig, ax = plt.subplots(1, 3, figsize=(14, 7))
+
+        best_model = adam
+        best_model_name = 'adam'
+        
+        if best_model.acc_[-1] < sgd.acc_[-1]:
+            best_model = sgd
+            best_model_name = 'sgd'
+        if best_model.acc_[-1] < RMSprop.acc_[-1]:
+            best_model = RMSprop
+            best_model_name = 'RMSprop'
+
+        save_model(best_model)
+
         print_metrics(ax, fig, adam, 'adam', save)
         print_metrics(ax, fig, sgd, 'sgd', save)
         print_metrics(ax, fig, RMSprop, 'RMSprop', save)
+
+        print(f'The {best_model_name} model is the best. So we keep its parameters')
+
         plt.show()
     elif sys.argv[1] == 'predict':
-        pass
+        hidden_layer, mean, std, parameters = load_model()
+
+        print(hidden_layer)
+        print(mean)
+        print(std)
+        for key, val in parameters.items():
+            print(f'{key}: {val.shape}')
+
+        
