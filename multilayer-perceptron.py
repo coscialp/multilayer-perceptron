01dd54c07fc0a1fd8d8d6004112d3483cdbab84d
Corentin Scialpi <coscialp@z2r12p6.42lyon.fr>
2022-05-19 18:18:09 +0200

    tanh add

--- multilayer-perceptron.py
+++ multilayer-perceptron.py
@@ -1,6 +1,8 @@
+from random import randint
 import sys, os
 import matplotlib.pyplot as plt 
 import numpy as np
+from scipy import rand
 from AI_learn.neural_network import MLPClassifier
 from AI_learn.dataset import load_dataset
 from AI_learn.preprocessing import StandartScaler, one_hot
@@ -98,13 +100,13 @@ def print_metrics(ax, fig, model, name_of_model, save):
         plt.savefig('metrics/figures.png')
 
     print(f'Metrics of {name_of_model}\'s model:')
-    print('-' * 100)
+    print('-' * 65)
     for epoch, loss in enumerate(model.loss_):
         line = f'epoch {(epoch + 1)}/{len(model.loss_)} - loss: {loss:.5f} - val_loss: {model.val_loss_[epoch]:.5f} - acc: {model.acc_[epoch]:.5f}'
         if save == True:
             file.write(f'{line}\n')
         print(line)
-    print('-' * 100)
+    print('-' * 65)
 
 
 def save_model(best_model):
@@ -112,6 +114,8 @@ def save_model(best_model):
         np.save(file, best_model.hidden_layers_)
         np.save(file, len(best_model.parameters_) // 2)
         np.save(file, best_model.normalize_)
+        np.save(file, best_model.activation_)
+        np.save(file, best_model.out_activation_)
         if best_model.normalize_ == True:
             np.save(file, best_model.normalize_mean_)
             np.save(file, best_model.normalize_std_)
@@ -126,11 +130,15 @@ def load_model():
     mean = None
     std = None
     parameters = {}
+    activation = ''
+    out_activation = ''
     try:
         with open('model.npy', 'rb') as file:
             hidden_layer = np.load(file)
             C = np.load(file)
             normalize = np.load(file)
+            activation = np.load(file)
+            out_activation = np.load(file)
             if normalize == True:
                 mean = np.load(file)
                 std = np.load(file)
@@ -141,7 +149,7 @@ def load_model():
         print('model.npy doesn\'t exist. Please try to launch a fit program!')
         exit(1)
 
-    return (hidden_layer, mean, std, parameters, normalize)
+    return (hidden_layer, mean, std, parameters, normalize, activation, out_activation)
 
 
 if __name__ == '__main__':
@@ -155,40 +163,37 @@ if __name__ == '__main__':
     y_onehot = one_hot(y).T
 
     if sys.argv[1] == 'fit':
-        adam = MLPClassifier(hidden_layers=(10, 10), n_iter=1000, learning_rate=0.001, normalize=True, early_stopping=False, multiclass=True)
-        adam.fit(X, y_onehot, solver='adam', batch_size=16)
+        adam = MLPClassifier(
+            hidden_layers=(16, 16),
+            n_iter=2000,
+            learning_rate_init=0.001,
+            normalize=True,
+            multiclass=True,
+            shuffle=False,
+            activation='tanh',
+            out_activation='softmax',
+            solver='adam',
+            batch_size=16,
+            )
 
-        # sgd = MLPClassifier(hidden_layers=(10, 10), n_iter=10000, learning_rate=0.001, normalize=True, early_stopping=False, multiclass=True)
-        # sgd.fit(X, y_onehot, solver='sgd', random_state=0)
-
-        # RMSprop = MLPClassifier(hidden_layers=(10, 10), n_iter=10000, learning_rate=0.001, normalize=True, early_stopping=False, multiclass=True)
-        # RMSprop.fit(X, y_onehot, solver='RMSprop', random_state=0)
+        adam.fit(X, y_onehot, random_state=0, test_size=0.1)
 
         fig, ax = plt.subplots(1, 3, figsize=(14, 7))
 
-        best_model = adam
-        best_model_name = 'adam'
-        
-        # if best_model.val_loss_[-1] > sgd.val_loss_[-1]:
-        #     best_model = sgd
-        #     best_model_name = 'sgd'
-        # if best_model.val_loss_[-1] > RMSprop.val_loss_[-1]:
-        #     best_model = RMSprop
-        #     best_model_name = 'RMSprop'
-
-        save_model(best_model)
+        save_model(adam)
 
         print_metrics(ax, fig, adam, 'adam', save)
-        # print_metrics(ax, fig, sgd, 'sgd', save)
-        # print_metrics(ax, fig, RMSprop, 'RMSprop', save)
-
-        print(f'The {best_model_name} model is the best. So we keep its parameters')
 
         plt.show()
     elif sys.argv[1] == 'predict':
-        hidden_layers, mean, std, parameters, normalize = load_model()
+        hidden_layers, mean, std, parameters, normalize, activation, out_activation = load_model()
 
-        model = MLPClassifier(hidden_layers, multiclass=True)
+        model = MLPClassifier(
+            hidden_layers,
+            multiclass=True,
+            activation=np.array_str(activation),
+            out_activation=np.array_str(out_activation)
+            )
 
         X = StandartScaler(X, mean, std)
 
